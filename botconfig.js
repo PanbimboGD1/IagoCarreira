24396cd66632730859fee1e05f505b620ce9c73e
asdasd <panbimbogd@gmail.com>
2025-02-25 03:02:16 +0100

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
+  Admins: ["285816872807694336", "UserID"], //Admins of the bot
   ExpressServer: true, //If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
-  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  SupportServer: "https://discord.gg/8532jdwC3m", //Support Server Link
+  Token: process.env.Token || "MTM0Mzc2MTY1MTg5NTExMTY5MQ.G0wwwl.DTGmGMtGm3ZGxw15aRt92VqJikaBGWxeOSTCxg", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1343761651895111691", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "ghCeOLPe3Pz9Al7oe-Kms26CYSvWTf3U", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   ServerDeafen: true, //If you want bot to stay deafened
   DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
@@ -21,22 +21,22 @@ module.exports = {
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: "Music", // The message shown
-    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+    name: "una operación de rodilla.", // The message shown
+    type: "STREAMING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "host name or IP",
-    port: 80, // The port that lavalink is listening to. This must be a number!
-    pass: "your password",
+    host: "server.relocation.tk",
+    port: 1032, // The port that lavalink is listening to. This must be a number!
+    pass: "youshallnotpass",
     secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "a568b55af1d940aca52ea8fe02f0d93b", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "e8199f4024fe49c5b22ea9a3dd0c4789", //Spotify Client Secret
   },
 };
